16aebb632c5741c97b3f2bb8c3e47dadfb28c921
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-09-28 11:44:24 -0400

    Create header

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './muralLogo.gif';
+import menu from "./menuIcon.gif";
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div className="header">
+        {this.props.menuButton &&
+          <a href="/menu">
+            <img src={menu} alt="Menu Icon" className="menuIcon"/>
+          </a>
+        }
+        <a href="/">
+          <img src={logo} alt="Header Image" className="headerImage"/>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Header;
