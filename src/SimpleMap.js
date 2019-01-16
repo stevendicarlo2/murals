dc00275b167837b02ccfa704db20edae4f5aee4e
Steven DiCarlo <stevendicarlo2@gmail.com>
2019-01-16 13:38:39 -0500

    Make map include all murals on load

--- src/SimpleMap.js
+++ src/SimpleMap.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { fitBounds } from 'google-map-react/utils';
 import MuralLabel from "./MuralLabel.js";
 import history from "./history";
 
 class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 38.89,
-      lng: -77.01
-    },
-    zoom: 15
-  };
   constructor(props) {
     super(props);
     const muralList = this.newMuralList(null);
+    const bounds = this.findMapBounds();
     this.state = {
       muralList: muralList,
+      mapBounds: bounds,
     };
   }
 
@@ -29,6 +25,43 @@ class SimpleMap extends Component {
     });
   }
 
+  findMapBounds() {
+    if (this.props.murals.length === 0) {
+      return {
+        center: {lat: 38.89, lng: -77.01},
+        zoom: 15
+      };
+    }
+    const firstMural = this.props.murals[0];
+    if (this.props.murals.length === 1) {
+      const center = { lat: firstMural.lat, lng: firstMural.lng };
+      const zoom = 16;
+      return {center: center, zoom: zoom};
+    }
+    const size = {width: window.innerWidth, height: window.innerHeight*.7};
+    let north = firstMural.lat;
+    let south = firstMural.lat;
+    let east = firstMural.lng;
+    let west = firstMural.lng;
+    this.props.murals.forEach(mural => {
+      if (mural.lat > north)
+        north = mural.lat;
+      if (mural.lat < south)
+        south = mural.lat;
+      if (mural.lng > east)
+        east = mural.lng;
+      if (mural.lng < west)
+        west = mural.lng;
+    });
+    const bounds = {
+      nw: { lat: north, lng: west },
+      se: { lat: south, lng: east }
+    };
+
+    const { center, zoom } = fitBounds(bounds, size);
+    return {center: center, zoom: zoom};
+  }
+
   newMuralList(hoverKey) {
     const muralList = this.props.murals.map(mural => {
       mural.hover = (mural.id === hoverKey);
@@ -36,6 +69,7 @@ class SimpleMap extends Component {
     });
     return muralList;
   }
+
   createLabelList() {
     return this.state.muralList.map((mural, i) => {
       return <MuralLabel
@@ -73,8 +107,8 @@ class SimpleMap extends Component {
       <div id="map">
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyDZyKqgqERLhKTwGTPjzLvQqi7lTFMBLN0" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={this.state.mapBounds.center}
+          defaultZoom={this.state.mapBounds.zoom}
           onChildClick={this.onChildClick}
           onChildMouseEnter={this.onChildMouseEnter}
           onChildMouseLeave={this.onChildMouseLeave}
